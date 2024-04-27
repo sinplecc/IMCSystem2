84a5aecd0b758ae49d43522b572e6cdd5cd59e75
SinpleCC <sinplecc@gmail.com>
2024-04-27 14:48:14 -0300

    consertando uns erros pequenos

--- imc.java
+++ imc.java
@@ -6,37 +6,37 @@ public class imc {
 
     
         System.out.println("Digite o nome: ");
-        String nome = scanner.nextLine();
+        String name = scanner.nextLine();
 
         System.out.println("Digite o gênero (M pra Masculino, F pra Feminino, N pra anônimo): ");
-        char genero = scanner.next().toUpperCase().charAt(0);
+        char gender = scanner.next().toUpperCase().charAt(0);
 
-        if (genero == 'N') {
-            genero = 'F';
+        if (gender == 'N') {
+            gender = 'F';
         }
 
         System.out.println("Digite a altura (em metros): ");
-        double altura = scanner.nextDouble();
+        double height = scanner.nextDouble();
 
         System.out.println("Digite o peso (em quilogramas): ");
-        double peso = scanner.nextDouble();
+        double weight = scanner.nextDouble();
 
-        double imc = peso / (altura * altura);
+        double imc = weight / (height * height);
      
-        String classificacao = getClassificacao(genero, imc);
+        String classification = getClassification(gender, imc);
 
-        System.out.println("\nNome: " + nome);
-        System.out.println("Gênero: " + (genero == 'M' ? "Masculino" : "Feminino"));
-        System.out.println("Altura: " + altura + " metros");
-        System.out.println("Peso: " + peso + " kg");
+        System.out.println("\nNome: " + name);
+        System.out.println("Gênero: " + (gender == 'M' ? "Masculino" : "Feminino"));
+        System.out.println("Altura: " + height + " metros");
+        System.out.println("Peso: " + weight + " kg");
         System.out.println("IMC: " + imc);
-        System.out.println("Classificação: " + classificacao);
+        System.out.println("Classificação: " + classification);
 
         scanner.close();
     }
 
-    public static String getClassificacao(char genero, double imc) {
-        if (genero == 'M') {
+    public static String getClassification(char gender, double imc) {
+        if (gender == 'M') {
             if (imc < 20) {
                 return "Abaixo do normal";
             } else if (imc < 25) {
